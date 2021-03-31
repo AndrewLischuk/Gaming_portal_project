680dc689e3957facb51440e90278937175836fe4
MarshallGunn <marshallgunn59@gmail.com>
2021-03-31 23:59:25 +0300

    Fixed mobile menus

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 
-//===========CONTACT US FORM===============
+//==============================CONTACT US FORM==============================
 
 let contactBtn = document.querySelector(".btn-compact");
 let closeBtn = document.querySelector(".btn-close");
 let formContactUs = document.querySelector(".contact-us");
 let line = document.querySelector(".line");
-console.log(formContactUs)
 
 
 contactBtn.addEventListener("click", function() {
@@ -28,3 +27,69 @@ closeBtn.addEventListener("click", function() {
     closeBtn.classList.remove("show");
     formContactUs.classList.remove("show", "transition-in");
 })
+
+//==============================MOBILE MENU==============================
+
+let mobileMenu = document.querySelectorAll(".nav-mobile-menu");
+let menu = document.querySelector(".menu");
+let featMenu = document.querySelector(".feat-menu");
+let blogMenu = document.querySelector(".blog-menu");
+
+let menuBtn = document.querySelectorAll(".menu a", ".feat-menu", ".blog-menu");
+
+
+
+   
+
+mobileMenu[0].addEventListener("click", function() {
+    mobileMenu[0].classList.toggle("active-menu");
+    if (mobileMenu[0].classList.contains("active-menu")) {
+        menu.classList.add("active-menu");
+    } else {
+        menu.classList.remove("active-menu")
+    }
+    menuBtn.forEach(btn => 
+        btn.addEventListener("click", function() {
+            mobileMenu[0].classList.toggle("active-menu");
+            menu.classList.remove("active-menu");
+        })
+    )
+})
+mobileMenu[1].addEventListener("click", function() {
+    mobileMenu[1].classList.toggle("active-menu");
+    if (mobileMenu[1].classList.contains("active-menu")) {
+        featMenu.classList.add("active-menu");
+    } else {
+        featMenu.classList.remove("active-menu")
+    }
+    menuBtn.forEach(btn => 
+        btn.addEventListener("click", function() {
+            mobileMenu[1].classList.toggle("active-menu");
+            featmenu.classList.remove("active-menu");
+        })
+    )
+})
+mobileMenu[2].addEventListener("click", function() {
+    mobileMenu[2].classList.toggle("active-menu");
+    if (mobileMenu[2].classList.contains("active-menu")) {
+        blogMenu.classList.add("active-menu");
+    } else {
+        blogMenu.classList.remove("active-menu")
+    }
+    menuBtn.forEach(btn => 
+        btn.addEventListener("click", function() {
+            mobileMenu[2].classList.toggle("active-menu");
+            blogmenu.classList.remove("active-menu");
+        })
+    )
+})
+
+
+
+
+
+
+
+
+
+
